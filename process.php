b483596f4484c6d6d8e40661e761eeeb53b45fba
marvin evins <maevins@cps.edu>
2018-03-24 13:32:57 -0500

    addedGetPostVar&getpostForms
    addedGetPostVar&getpostForms

--- process.php
+++ process.php
@@ -2,6 +2,13 @@
 
 echo 'processing ....';
 
+//CHECK FOR POST variable
+if(isset($_POST['name'])){
+  echo 'POST: Your name is '. $_POST['name'];
+}
+
+
+//check for get variable
 if(isset($_GET['name'])){
   echo 'GET: Your name is '. $_GET['name'];
 }
